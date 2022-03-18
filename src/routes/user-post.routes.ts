5428d5007a84d76e473b67e14fb5ca41a879ec29
Huy Chuong <chuonglh5798@gmail.com>
2022-03-18 15:35:19 +0700

    delete routes, controller, service list postregister

--- src/routes/user-post.routes.ts
+++ src/routes/user-post.routes.ts
@@ -3,10 +3,10 @@ import UserPostController from "../controller/user-post.controller";
 export default (app) => {
   app
     .get("/user-post/stats", UserPostController.getPostStats)
-    .get("/user-post/registered", UserPostController.listRegisteredRequests)
     .put("/update-user-status", UserPostController.updatePostStatus)
     .get(
       "/get-register-detail",
       UserPostController.countRegisterOfPost
     )
+
 };
