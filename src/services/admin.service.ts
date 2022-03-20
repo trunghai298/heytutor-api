9cedd9111ffafb0796b310041bf60fd4d4c9ecc4
Huy Chuong <chuonglh5798@gmail.com>
2022-03-21 00:29:47 +0700

    add / update ctv

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,18 +1,109 @@
-import { BadRequestError } from "../utils/errors";
 import Admin from "../models/admin.model";
+import Activity from "../models/activity.model";
+import usersService from "./users.service";
 
-/**
- * To create a new class
- */
-export const fetch = async (ctx) => {
+const addCollaborator = async (ctx, payload) => {
+  const { email, password, name, role, permission } = payload;
+  const userId = ctx?.user?.id || 2;
   try {
-    const userId = ctx?.user?.id;
-    const res = await Admin.findOne({ where: { id: userId } });
-    return res;
-  } catch (error) {
-    throw new BadRequestError({
-      field: "id",
-      message: "Failed to create this item.",
+    const user = await Admin.findOne({
+      where: { email },
+      raw: true,
     });
+    if (user === null) {
+      const res = await Admin.create({
+        email,
+        password,
+        name,
+        role,
+        permission,
+      });
+      const username = await (
+        await Admin.findOne({
+          where: {
+            id: userId,
+          },
+        })
+      ).name;
+      const log = await Activity.create({
+        userId,
+        username,
+        action: "add",
+        content: `new collaborator ${name}`,
+      });
+      return {
+        log,
+      };
+    } else {
+      return {
+        message: "User already existed",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return error;
   }
+};
+
+const updateCollaborator = async (ctx, payload) => {
+  const { email, password, name, role, permission } = payload;
+  const userId = ctx?.user?.id || 2;
+  try {
+    const admin = await Admin.findAll({
+      where: { email },
+    });
+    if (admin.length !== 0) {
+      const res = await Admin.update(
+        {
+          name,
+          role,
+          permission,
+        },
+        {
+          where: {
+            email,
+          },
+        }
+      );
+      const username = await (
+        await Admin.findOne({
+          where: {
+            id: userId,
+          },
+        })
+      ).name;
+      const log = await Activity.create({
+        userId,
+        username,
+        action: "update",
+        content: `collaborator ${name}`,
+      });
+      return {
+        log,
+      };
+    } else {
+      return {
+        message: "User did not existed",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+const listAllCollaborator = async () => {
+  try {
+    const listCollaborator = await Admin.findAll();
+    return listCollaborator;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+export default {
+  addCollaborator,
+  updateCollaborator,
+  listAllCollaborator,
 };
