4b39507232939d16154d941c8b68a9df591ac1e9
haintse06350 <haint180298@gmail.com>
2022-04-29 15:56:34 +0700

    hot fix env

--- src/clients/mysql.ts
+++ src/clients/mysql.ts
@@ -1,18 +1,23 @@
 import { Sequelize } from "sequelize";
 
-const MySQLClient = new Sequelize("heytutor-test", "admin", "kbdZZeiS", {
-  host: "10518",
-  port: 10518,
-  dialect: "mysql",
-  pool: {
-    max: 110,
-    min: 0,
-    acquire: 1000000,
-    idle: 200000,
-  },
-  dialectOptions: { decimalNumbers: true, multipleStatements: true },
-  logging: false, //console.log,
-  timezone: "+07:00",
-});
+const MySQLClient = new Sequelize(
+  "heytutor-test",
+  process.env.DB_USER || "admin",
+  process.env.DB_PASSWORD || "kbdZZeiS",
+  {
+    host: process.env.DB_HOST || "10518",
+    port: parseInt(process.env.DB_PORT || "", 10) || 3306,
+    dialect: "mysql",
+    pool: {
+      max: 110,
+      min: 0,
+      acquire: 1000000,
+      idle: 200000,
+    },
+    dialectOptions: { decimalNumbers: true, multipleStatements: true },
+    logging: false, //console.log,
+    timezone: "+07:00",
+  }
+);
 
 export default MySQLClient;
