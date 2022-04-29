fa4d23e60e7ddb715a97f41b2de788d1e7017114
haintse06350 <haint180298@gmail.com>
2022-04-29 22:34:46 +0700

    fix jwt

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,6 +1,7 @@
 import NotificationController from "../controller/notification.controller";
 
 export default (app) => {
+  console.log("NotificationController");
   app.get("/notifications", NotificationController.listNotification);
   app.put("/notifications/read", NotificationController.readNoti);
 };
